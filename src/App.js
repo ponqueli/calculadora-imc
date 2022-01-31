932b468ff4221cf22fdc96b869d19e0082f9e025
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-31 15:10:27 -0300

    criando calc imc estudar useState reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC(){
+    const alt = altura/100;
+    const imc = peso/ (alt * alt);
+
+    if( imc < 18.6 ){
+      setMensagem(`Seu IMC é de ${ imc.toFixed(2) }, abaixo do peso!`,  );
+    }else if(imc >= 18.6 && imc < 24.9 ){
+      setMensagem(`Seu IMC é de ${ imc.toFixed(2) }, no peso ideal!`);
+    }else if(imc >= 24.9 && imc < 34.9 ){
+      setMensagem(`Seu IMC é de ${ imc.toFixed(2) }, levemente acima do peso!`);
+    }else if(imc >=34.9){
+      setMensagem(`Seu IMC é de ${ imc.toFixed(2) }, acima do peso! Cuidado com a Obesidade!`);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+
+      <div className="area-input">
+        <input
+          type="text"
+          placeholder="Informe o peso em (kg) Ex: 90"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Informe a altura em (cm) Ex: 180"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+          onKeyUp={(e) => (e.key === "Enter" ? calcularIMC() : {})}
+        />
+
+        <button onClick={calcularIMC}>Calcular</button>
+      </div>
+
+      <h2>{ mensagem }</h2>
     </div>
   );
 }
-
-export default App;
